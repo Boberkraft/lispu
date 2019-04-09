f5b39db161b9476c988efe249d113e06a27bb936
Andrzej Bisewski <andrzej.bisewski@gmail.com>
2019-04-09 22:48:56 +0200

    foundation for server

---
+++ projekty/testing-rendering/communication.lisp
@@ -0,0 +1,35 @@
+(defpackage #:communication
+  (:use #:cl)
+
+  (:export :start-simple-server
+           :start-simple-client))
+
+(defparameter *connection-id* 0)
+
+(defun make-id (&rest lst)
+  ;;TODO: it should be replecable/changeble. Maybe it is?
+  "Converts given list to string. Used as id"
+  (format nil "~{~a~}" lst) )
+
+
+(defun start-simple-server (port callback)
+  "Listen on port for messange, and call callback with recived input"
+  (usocket:with-socket-listener (socket "127.0.0.1" port)
+    (usocket:wait-for-input socket)
+    (usocket:with-connected-socket (connection (usocket:socket-accept socket))
+      (let ((addr (usocket:get-peer-address connection))
+            (port (usocket:get-peer-port connection)))
+        (funcall callback (make-id addr port) (string-trim '(#\Space #\Newline)
+                                          (read-line (usocket:socket-stream connection))))))))
+
+
+;; (bt:make-thread (lambda ()
+;;                   (start-simple-server 5512 '(lambda (a b) (print (list a b))))
+;;                    (print "ENDED")))
+
+
+(defun start-simple-client (port)
+  "Connect to a server and send a messange."
+  (usocket:with-client-socket (socket stream "127.0.0.1" port)
+    (format stream "Hello world!~%")
+    (force-output stream)))
